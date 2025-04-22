47b8113bcb654260eddab7adcdd18f685e592bd0
Meredith Moore <Meredith.Moore@drake.edu>
2025-04-22 09:02:54 -0500

    Create sendJoke.py

---
+++ sendJoke.py
@@ -0,0 +1,17 @@
+# Author: Meredith Moore
+# Description: A Basic SNS test
+
+import boto3
+
+sns = boto3.client('sns')
+topic_arn = 'arn:aws:sns:us-east-2:REPLACE_WITH_YOUR_TOPIC_ARN'
+
+message = "Here's your joke of the day!"
+
+response = sns.publish(
+    TopicArn=topic_arn,
+    Message=message,
+    Subject='Joke of the Day'
+)
+
+print("Notification sent!")
