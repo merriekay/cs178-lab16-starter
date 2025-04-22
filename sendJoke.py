216bc29eea8f69b365cbcb295da910f3a26bb24b
Meredith Moore <Meredith.Moore@drake.edu>
2025-04-22 09:05:07 -0500

    Update sendJoke.py

--- sendJoke.py
+++ sendJoke.py
@@ -4,7 +4,7 @@
 import boto3
 
 sns = boto3.client('sns')
-topic_arn = 'arn:aws:sns:us-east-2:REPLACE_WITH_YOUR_TOPIC_ARN'
+topic_arn = 'arn:aws:sns:us-east-2:REPLACE_WITH_YOUR_TOPIC_ARN' # ← Replace with your SNS topic ARN
 
 message = "Here's your joke of the day!"
 
