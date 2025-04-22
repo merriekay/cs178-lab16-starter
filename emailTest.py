1bcb9ce5b4a79487dff07086db871f6e9fee5046
Meredith Moore <Meredith.Moore@drake.edu>
2025-04-22 09:04:55 -0500

    Create emailTest.py

---
+++ emailTest.py
@@ -0,0 +1,27 @@
+# Author: Meredith Moore
+# Description: Pull from DynamoDB + send via SNS
+
+import boto3
+import random
+
+# Set up DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Lab16Jokes')
+
+# Scan the table and select a random joke
+response = table.scan()
+items = response['Items']
+random_joke = random.choice(items)['joke']
+
+# Set up SNS client
+sns = boto3.client('sns')
+topic_arn = 'arn:aws:sns:us-east-2:REPLACE_WITH_YOUR_TOPIC_ARN' # ← Replace with your SNS topic ARN
+
+# Send the joke via SNS
+response = sns.publish(
+    TopicArn=topic_arn,
+    Message=random_joke,
+    Subject='Joke of the Day'
+)
+
+print("Joke sent via SNS!")
